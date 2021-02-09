9034c3fd9e578afdeae469b102e4431bf1108762
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-02-08 23:28:06 -0600

    Created the workout mongoose schema

---
+++ models/workout.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        name: {
+            type: String,
+            trim: true,
+            required: "Name of the exercise is required."
+        },
+        exerciseType: {
+            type: String,
+            trim: true,
+            required: "Exercise type is required."
+        },
+        weight: {
+            type: Number,
+            trim: true,
+            required: "Enter your weight is required in order for us to track your daily workout progress."
+        },
+        sets: {
+            type: Number,
+            trim: true
+        },
+        reps: {
+            type: Number,
+            trim: true
+        },
+        duration: {
+            type: Number,
+            trim: true,
+            required: "Enter your workout duration for today in order for us to track your daily workout progress."
+        },
+        distance: {
+            type: Number,
+            trim: true
+        }
+    }]
+});
+
