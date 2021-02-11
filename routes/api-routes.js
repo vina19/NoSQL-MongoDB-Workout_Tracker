12ac3bc24cf2c454d2232d43f0070e310ad9ddde
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-02-10 19:05:06 -0600

    Find all the exercises and display it

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,16 @@
 const db = require("../models");
 
-module.exports = function(app) {
-
+module.exports = function (app) {
+    
+  // GET ROUTES
+  // GET all the exercises
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch(({ message }) => {
+        console.log(message);
+      });
+  });
 };
