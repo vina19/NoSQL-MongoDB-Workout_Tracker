c38ce2fbff49770688983c49f41f42e035ca3c5e
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-02-10 19:01:59 -0600

    Render the stats.html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,5 +11,10 @@ module.exports = function(app) {
     // Send the user to the exercise page where the user can submit their workout for the day
     app.get("/exercise", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    })
+    });
+
+    // Send the user to the app dashboard
+    app.get("/stats", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
+    });
 };
