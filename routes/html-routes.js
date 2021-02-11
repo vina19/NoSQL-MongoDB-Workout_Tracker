190c05c6e812cc4f61ea4dc910810503cfaebda7
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-02-10 19:00:24 -0600

    Render the exercise.html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,11 +3,13 @@ const path = require("path");
 module.exports = function(app) {
 
     // HTML ROUTES
-    // Send the user to the index page
+    // Send the user to the main page
     app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
-    
-
+    // Send the user to the exercise page where the user can submit their workout for the day
+    app.get("/exercise", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    })
 };
