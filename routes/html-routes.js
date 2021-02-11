3220b622df4b0d8e5f3be8f84814d0bf7078cec3
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-02-10 18:56:31 -0600

    Render the index.html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,4 +2,12 @@ const path = require("path");
 
 module.exports = function(app) {
 
+    // HTML ROUTES
+    // Send the user to the index page
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    });
+
+    
+
 };
