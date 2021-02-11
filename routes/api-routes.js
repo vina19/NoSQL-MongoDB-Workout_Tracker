61c715288cdfc7188e45fba1715d7ad269576e73
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-02-10 19:10:15 -0600

    Find the exercises in range

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,10 +1,20 @@
 const db = require("../models");
 
 module.exports = function (app) {
-    
   // GET ROUTES
   // GET all the exercises
   app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch(({ message }) => {
+        console.log(message);
+      });
+  });
+
+  // GET exercises in range
+  app.get("/api/workouts/range", (req, res) => {
     db.Workout.find({})
       .then((dbWorkout) => {
         res.json(dbWorkout);
